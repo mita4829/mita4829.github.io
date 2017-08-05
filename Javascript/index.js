1a4869f13ac548c3d12b874911c713526e58c47f
Michael <michael.tang-1@colorado.edu>
2017-08-05 16:59:50 -0600

    css effects

--- Javascript/index.js
+++ Javascript/index.js
@@ -10,10 +10,21 @@ function parallax(){
             //document.getElementById('z').innerHTML = z;
             document.getElementById('icons').style.left = x/3+"px";
             document.getElementById('icons').style.top = -10+y/3+"px";
+            document.getElementById('parallax').style.left = -(x/2)+"px";
+
         }, true);
     }
 }
 
+function lackParallax(e){
+    var x = e.clientX;
+    var y = e.clientY;
+    document.getElementById('icons').style.left = x/80+"px";
+    document.getElementById('icons').style.top = -10+y/80+"px";
+    document.getElementById('parallax').style.left = -(x/160)+"px";
+    var coor = "Coordinates: (" + x + "," + y + ")";
+    document.getElementById("x").innerHTML = coor;
+}
 var switchPosition = false;
 function presentSideView(){
     if(!switchPosition){
@@ -35,21 +46,21 @@ function checkIfMobile(){
 
 function navigationBarView(mobile){
   if(mobile){
-    document.getElementById("navBar").style.height = "10%";
-    document.getElementById("navBar").style.minHeight = "80px";
+    document.getElementById("navBar").style.height = "90px";
+    document.getElementById("navBar").style.minHeight = "90px";
 
-    document.getElementById("sideButton").style.height = "80%";
+    //document.getElementById("sideButton").style.height = "80%";
 
   }else{
-    document.getElementById("navBar").style.height = "10%";
-    document.getElementById("navBar").style.minHeight = "60px";
+    document.getElementById("navBar").style.height = "12%";
+    document.getElementById("navBar").style.minHeight = "70px";
 
-    document.getElementById("sideButton").style.height = "75%";
+    //document.getElementById("sideButton").style.height = "75%";
   }
 }
 
 function main(){
   var mobile = checkIfMobile();
   navigationBarView(mobile);
-  //parallax();
+  parallax();
 }
